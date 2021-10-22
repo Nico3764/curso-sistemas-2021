207876257a9e0131b37cfd1ccb22371adc144511
Nicolas Pablo Robles <nicolasroble@gmail.com>
2021-10-22 02:55:45 -0300

    crud2.0

---
+++ bloque8/CRUD2.0/Backend/user/create.php
@@ -0,0 +1,17 @@
+<?php
+
+$response = 
+[
+    ["id", "username", "password"],
+    [1, "root", "123456"],
+    [2, "juan", "re78934"],
+    [3, "lorena", "******"],
+    [4, "ariel", "aka8932j"],
+    [5, "florencia", "jhsqnf6"],
+    [6, "nicolas", "99os9si"],
+    [7, "new", "123454321"]
+];
+
+echo json_encode($response);
+
+?>
---
+++ bloque8/CRUD2.0/Frontend/main.js
@@ -0,0 +1,290 @@
+let read = () =>
+{
+    let connection = new XMLHttpRequest();
+
+    connection.open('GET', '../Backend/user/read.php');
+
+    connection.addEventListener('loadend', event =>{ processServerResponse(event, 'read') } );
+    connection.send();
+}
+
+let create = (data) =>
+{
+    let connection = new XMLHttpRequest();
+
+    connection.open('POST', '../Backend/user/create.php');
+
+    connection.addEventListener('loadend', event =>{ processServerResponse(event, 'create') } );
+    connection.send( JSON.stringify(data) );
+}
+
+let edit = ( data ) =>
+{
+    let connection = new XMLHttpRequest();
+
+    connection.open('POST', '../Backend/user/edit.php');
+
+    connection.addEventListener('loadend', event =>{ processServerResponse(event, 'edit') } );
+    connection.send( JSON.stringify(data) );
+}
+
+let remove = (data) =>
+{
+    let connection = new XMLHttpRequest();
+
+    connection.open('POST', '../Backend/user/remove.php');
+
+    connection.addEventListener('loadend', event =>{ processServerResponse(event, 'remove') } );
+    connection.send( JSON.stringify(data) );
+}
+
+let HTMLUserTable = ( data ) =>
+{
+    let HTMLCode = `<table class="w3-margin-top w3-margin-left">`
+
+    for ( let row=0; row<data.length; row++)
+    {
+        HTMLCode += `<tr id='${data[row][0]}'>`;
+
+        for (let column=0; column<data[row].length; column++)
+        {
+            HTMLCode += `<td>${data[row][column]}</td>`;
+        }
+
+        if ( row == 0 )
+        {
+            HTMLCode += `<td>actions</td>`;
+        }
+        else
+        {
+            HTMLCode += `<td>
+                        <button name="edit">Editar</button>
+                        <button name="remove">Borrar</button>
+                    </td>`;
+        }
+
+        HTMLCode += `</tr>` ;                                   
+    }                       
+                        
+    HTMLCode += `</table>`;
+    HTMLCode += `<button name="create" class="w3-margin-top w3-margin-left">new user</button>`;
+    return HTMLCode;
+}
+
+let HTMLCreateFormUserDialog = () =>
+{
+    let HTMLCode =
+        ` <div class="w3-container w3-card-4 w3-light-grey w3-padding-16">
+            <h2 class="w3-center" style='font-weight: bold'>NUEVO USUARIO</h2>
+            <label>Name*</label><br>
+            <input id ='username' class="w3-input w3-border w3-round-large w3-text-grey" type="text" name="Name" value="enter new name"><br>
+            <label>Password*</label><br>
+            <input id ='password' class="w3-input w3-border w3-round-large w3-text-grey" type="password" name="pass" value="enter new password"><br>
+        `;
+
+    let getFormData = () =>
+    {
+        let data =
+        {
+            username: document.getElementById('username').value,
+            password: document.getElementById('password').value
+        }
+
+        return data;
+    }
+ 
+    let dialog =
+    {
+        HTMLDialogView: HTMLCode,
+        formData: getFormData
+    }
+
+    return dialog;
+}
+
+let HTMLEditFormUserDialog = (id) =>
+{
+    let element = document.getElementById(id);
+
+    let HTMLCode =
+    `<div class="w3-container w3-card-4 w3-light-grey w3-padding-16">
+        <h2 class="w3-center" style='font-weight: bold'>Edit User ID=${element.childNodes[0].innerText }</h2>
+        <label>Name*</label><br>
+        <input id ='username' class="w3-input w3-border w3-round-large w3-text-grey" type="text" name="Name" value="${element.childNodes[1].innerText }"><br>
+        <label>Password*</label><br>
+        <input id ='password' class="w3-input w3-border w3-round-large w3-text-grey" type="password" name="pass" value="${element.childNodes[2].innerText }"><br>
+    `;
+
+    let getFormData = () =>
+    {
+        let data =
+        {
+            username: document.getElementById('username').value,
+            password: document.getElementById('password').value
+        }
+
+        return data;
+    }
+ 
+    let dialog =
+    {
+        HTMLDialogView: HTMLCode,
+        formData: getFormData
+    }
+
+    return dialog;
+}
+
+let HTMLRemoveFormUserDialog = (id) =>
+{
+    let element = document.getElementById(id);
+
+    let HTMLCode =
+    `<div class="w3-container w3-card-4 w3-light-grey w3-padding-16">
+        <p class="w3-wide w3-center w3-xlarge w3-red w3-padding-32 w3-round-xlarge ">BORRAR USER: ${element.childNodes[1].innerText }, ID: ${element.childNodes[0].innerText } </p>`;
+
+    let getFormData = () =>
+    {
+        let data =
+        {
+            id: element.childNodes[0].innerText
+        }
+
+        return data;
+    }
+ 
+    let dialog =
+    {
+        HTMLDialogView: HTMLCode,
+        formData: getFormData
+    }
+
+    return dialog;
+}
+let showModalDialog = ( HTMLDialogFormElement, confirmAction, cancelAction ) =>
+{
+    let modalElement = document.getElementById('modalDialogView');
+   
+    modalElement.innerHTML =
+    `<div class="w3-modal-content w3-animate-top">
+        <header class="w3-container w3-teal"> 
+        <span id="cancel" class="w3-button w3-display-topright">&times;</span>
+    </header>
+    ${ HTMLDialogFormElement.HTMLDialogView } 
+        <button id="confirm">Accept</button> 
+        </div>
+    </div>`; 
+                                                //HTMLDialogFormElement.HTMLDialogView es el código html generado en la function HTMLCreateFormUserDialog.
+    let confirmButtonClick = (event) =>
+    {
+        if ( confirmAction != undefined || confirmAction != null )
+            confirmAction(HTMLDialogFormElement.formData());
+        
+        modalElement.style.display = 'none';
+        modalElement.removeEventListener('click',confirmButtonClick);
+        modalElement.removeEventListener('click',cancelButtonClick);
+    }
+
+    let cancelButtonClick = (event) =>
+    {
+        if ( cancelAction != undefined || cancelAction != null )
+            cancelAction(HTMLDialogFormElement.formData());
+                
+        modalElement.style.display = 'none';
+        modalElement.removeEventListener('click',confirmButtonClick);
+        modalElement.removeEventListener('click',cancelButtonClick);
+    }
+
+    document.getElementById('confirm').addEventListener('click', confirmButtonClick );
+    document.getElementById('cancel').addEventListener('click', cancelButtonClick );
+
+    modalElement.style.display = 'block';
+}
+
+
+let processActionButtonClickEvents=(event) =>
+{
+    let element = event.target; //me trae el elemento que generó el evento.
+    let actionName = element.name;//me trae el atributo 'name' del elemento que generó el evento.
+    let elementId = element.parentElement.parentElement.id;//me trae el id de la fila.
+
+    switch (actionName)
+    {
+        case 'create':
+            let createDialog = HTMLCreateFormUserDialog(); //la función HTMLCreateFormUserDialog() devuelve un objet con el html de la ventana modal y los valores de user y pass.
+            console.log('create request action for new server id');
+            showModalDialog(createDialog, create, null ); //llama a la función que muestra/oculta la ventana modal.
+        break;
+
+        case 'edit':
+            let editDialog = HTMLEditFormUserDialog(elementId);
+            console.log('edit request action for data id='+elementId);
+            showModalDialog(editDialog, edit, null );
+        break;
+
+        case 'remove':
+            let removeDialog = HTMLRemoveFormUserDialog(elementId);
+            console.log('remove request action for data id='+elementId);
+            showModalDialog(removeDialog, remove, null );
+        break;
+
+        default:
+    }
+}
+
+
+let UpdateView=(data) =>
+{
+    
+    let viewElement = document.getElementById('userTable');
+    viewElement.removeEventListener('click', processActionButtonClickEvents);
+
+    viewElement.innerHTML = HTMLUserTable( data ); //Inserta el codigo HTML genegado por la función HTMLUserTable a la que le paso por parámetro la tabla (data). 
+
+    viewElement.addEventListener('click', processActionButtonClickEvents ); //asocia el detector de eventos (addEventListener al evento'click') sobre el objeto viewElement
+                                                                            //que esta sociado (document.getElementById("userTable")) al elemento con id ="userTable      
+}                                                                           //disparando la función processActionButtonClickEvents. 
+
+                                                                                
+let processServerResponse = (event, name) =>
+{
+    if ( event.currentTarget.status == 200 )
+    {
+    
+        let serverResponse = event.currentTarget.responseText; //event.currentTarget trae el elemento que generó el evento, en este caso el objeto de la conexion.
+                                                               //event.currentTarget.responseText trae la respuesta del servidor en formato texto (JSON en este caso).
+        data = JSON.parse(serverResponse);//Desserealiza la respuesta del servidor(JSON-->Array en este caso) y la asigna a la vble data.
+
+        switch (name)
+        {
+            case 'read':
+                UpdateView(data);
+            break;
+            case 'create':
+                UpdateView(data);
+                read();
+            break;
+            case 'edit':
+                UpdateView(data);
+                read();
+            break;
+            case 'remove':
+                UpdateView(data);
+                read();
+            break;
+
+            default:
+        }
+    }
+    else
+    {
+        alert("Hubo errores al procesar la solicitud.");
+    }
+}
+
+let start =() =>
+{
+    read();
+}
+
+window.addEventListener('DOMContentLoaded', start );
